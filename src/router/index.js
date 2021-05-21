ef73db46e5594605658a6bf8ba086e0f05f95fb5
ofg <1309870473@qq.com>
2021-05-21 13:17:59 +0800

    add: 构建页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import indexPage from '@/page/index'
+
 
 Vue.use(Router)
 
@@ -8,8 +9,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: indexPage,
+      mate: {title: '主页'}
     }
-  ]
+    ]
 })
